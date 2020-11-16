579d4e5374b5b2a89771db159784ce9ccdc22608
ccsunnyfd <chzhyfd@126.com>
2020-11-16 10:37:31 +0800

    [add] template method

---
+++ src/behavioral/templateMethod/templateMethod.go
@@ -0,0 +1,103 @@
+package main
+
+import "fmt"
+
+// iOtp interface
+type iOtp interface {
+	genRandomOTP(int) string
+	saveOTPCache(string)
+	getMessage(string) string
+	sendNotification(string) error
+	publishMetric()
+}
+
+// abstract otp
+type abstractOtp struct {
+	iOtp
+}
+
+func (o *abstractOtp) genAndSendOTP(otpLength int) error {
+	otp := o.iOtp.genRandomOTP(otpLength)
+	o.iOtp.saveOTPCache(otp)
+	message := o.iOtp.getMessage(otp)
+	err := o.iOtp.sendNotification(message)
+	if err != nil {
+		return err
+	}
+	o.iOtp.publishMetric()
+	return nil
+}
+
+// sms concrete otp
+type sms struct {
+	otp abstractOtp
+}
+
+func (s *sms) genRandomOTP(len int) string {
+	randomOTP := "1234"
+	fmt.Printf("SMS: generating random otp %s\n", randomOTP)
+	return randomOTP
+}
+
+func (s *sms) saveOTPCache(otp string) {
+	fmt.Printf("SMS: saving otp: %s to cache\n", otp)
+}
+
+func (s *sms) getMessage(otp string) string {
+	return "SMS OTP for login is " + otp
+}
+
+func (s *sms) sendNotification(message string) error {
+	fmt.Printf("SMS: sending sms: %s\n", message)
+	return nil
+}
+
+func (s *sms) publishMetric() {
+	fmt.Printf("SMS: publishing metrics\n")
+}
+
+// email concrete otp
+type email struct {
+	otp abstractOtp
+}
+
+func (s *email) genRandomOTP(len int) string {
+	randomOTP := "1234"
+	fmt.Printf("EMAIL: generating random otp %s\n", randomOTP)
+	return randomOTP
+}
+
+func (s *email) saveOTPCache(otp string) {
+	fmt.Printf("EMAIL: saving otp: %s to cache\n", otp)
+}
+
+func (s *email) getMessage(otp string) string {
+	return "EMAIL OTP for login is " + otp
+}
+
+func (s *email) sendNotification(message string) error {
+	fmt.Printf("EMAIL: sending email: %s\n", message)
+	return nil
+}
+
+func (s *email) publishMetric() {
+	fmt.Printf("EMAIL: publishing metrics\n")
+}
+
+// main 利用嵌套接口的struct的技巧，非常值得借鉴。
+// 如果直接调用子类sms的genAndSendOTP方法，是找不到该方法的。
+// 因为子类仅仅嵌套组合了abstractOtp,并不代表它实现了iOtp接口
+func main() {
+	smsOTP := &sms{}
+	o := abstractOtp{
+		iOtp: smsOTP,
+	}
+	o.genAndSendOTP(4)
+
+	fmt.Println("")
+	emailOTP := &email{}
+	o = abstractOtp{
+		iOtp: emailOTP,
+	}
+	o.genAndSendOTP(4)
+}
